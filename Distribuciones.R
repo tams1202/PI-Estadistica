c5496bcc90cdaaf1fe0c738c77129216604da16c
tams1202 <100395856+tams1202@users.noreply.github.com>
2022-05-09 08:22:19 -0500

    Distribuciones

---
+++ Distribuciones.R
@@ -0,0 +1,42 @@
+
+# ----------------Distribución--------------------
+
+
+# Probabilidad de que 35 alumnos lleguen en 15 minutos a la entrada de la
+# escuela.
+# x=numero de alumnos
+# lamda=tiempo de llegada.
+
+depois(x,lamda)
+dpois(35,15)
+ 
+
+# Probabilidad de que 50 alumnos lleguen en 15 minutos a la entrada de la
+# escuela.
+
+dpois(50,15)
+# 0.000000000000641
+
+# Probabilidad de que 34 alumnos lleguen en 5 minutos a la entrada de la
+# escuela.
+
+dpois(34,5)
+
+# 0.0000000000000000132
+
+# Probabilidad de que 60 alumnos lleguen en 5 munitos a la entrada de la
+# escuela.
+
+dpois(60,5)
+
+# Probabilidad de que 120 alumnos lleguen en 5 minutos a la entrada de la
+# escuela.
+
+dpois(120,5)
+
+#-----------------------------------------
+#         CONCLUSION
+#-----------------------------------------
+
+# En conclusión, se puede decir que para evitar aglomeraciones se debe dar
+# mayor tiempo de llegada a las escuelas.
